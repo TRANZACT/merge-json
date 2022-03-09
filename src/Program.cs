99cae852b29541cd5ab6561f46ce86af33b63a02
Michael Robertson <michael.robertson@tranzact.net>
2022-03-09 10:55:38 -0500

    Created src and test projects

---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string? sourceFilePath = args?.ElementAtOrDefault(0);
+
+if (sourceFilePath is null)
+{
+    Console.Error.WriteLine("Source file path was not provided");
+    return -2;
+}
+
+string? targetFilePath = args?.ElementAtOrDefault(1);
+if (targetFilePath is null)
+{
+    Console.Error.WriteLine("Target json file path was not provided");
+    return -3;
+}
+
+var mergeTask = new MergeJson.MergeJsonFiles(sourceFilePath, targetFilePath);
+bool isSuccess = mergeTask.Execute();
+
+if (!isSuccess)
+{
+    Console.Error.WriteLine("Failed to merge json files.");
+    return -1;
+}
+
+Console.WriteLine("Successfully merged json files.");
+return 0;
+
